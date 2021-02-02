242f44763da9d74fcd231ba483a3ea7614e40818
Miguel Angelo D. Ildefonso <migs.ildefonso@JPI.Local>
2021-02-02 11:17:34 +0800

    my first project commited

---
+++ mysample/src/org/fujitsu/training/codes/Welcome.java
@@ -0,0 +1,8 @@
+package org.fujitsu.training.codes;
+
+public class Welcome{
+
+	public static void main(String[] args){
+		System.out.println("Welcome, trainees!!!");
+	}
+}
---
+++ mysample/src/org/fujitsu/training/test/Easy.java
@@ -0,0 +1,6 @@
+public class Easy{
+
+	public static void main(String[] args){
+		System.out.println("This java is easy!Chicken!");
+	}
+}
